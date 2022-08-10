7945742f0b966250b39c0ccdc721ef8c64a9eba0
Anotonina <covushka@inbox.ru>
2022-08-10 15:13:02 +0300

    Diagrams size add
    Diagrams size add

--- MyFirstWebApp/wwwroot/js/Diagrams.js
+++ MyFirstWebApp/wwwroot/js/Diagrams.js
@@ -1,7 +1,4 @@
 ﻿var popCanvas = $("#popChart");
-var popCanvas = document.getElementById("popChart");
-var popCanvas = document.getElementById("popChart").getContext("2d");
-
 
 let shopsData = [];
 for (var i = 0; i < model.shops.length; i++) {
@@ -17,6 +14,29 @@ console.log(shopIncomeData);
 
 var barChart = new Chart(popCanvas, {
     type: 'bar',
+    data: {
+        labels: shopsData,
+        datasets: [{
+            label: 'Доходы магазинов',
+            data: shopIncomeData,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(54, 162, 235, 0.6)',
+                'rgba(255, 206, 86, 0.6)',
+                'rgba(75, 192, 192, 0.6)',
+                'rgba(153, 102, 255, 0.6)',
+                'rgba(255, 159, 64, 0.6)',
+            ]
+        }]
+    },
+
+});
+
+
+var popCanvasPie = $("#popChartPie");
+
+var pieChart = new Chart(popCanvasPie, {
+    type: 'pie',
     data: {
         labels: shopsData,
         datasets: [{
@@ -36,5 +56,23 @@ var barChart = new Chart(popCanvas, {
                 'rgba(153, 102, 255, 0.6)'
             ]
         }]
+    },
+    options: {
+        tooltips: {
+            enabled: false
+        },
+        plugins: {
+            datalabels: {
+                color: '#111',
+                textAlign: 'center',
+                font: {
+                    lineHeight: 1.6
+                },
+                formatter: function (value) {
+                    return value;
+                }
+                }
+            }
+        
     }
 });
